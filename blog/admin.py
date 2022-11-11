a600685ec3849c862534d43a41a71c0705a93e08
psnaz <psnazle@gmail.com>
2022-11-11 11:51:06 +0000

    Add approval action to the admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -19,3 +19,7 @@ class CommentAdmin(admin.ModelAdmin):
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
     search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
