c281852c8f24344b59d73a96867819a0aab63ed4
psnaz <psnazle@gmail.com>
2022-11-11 10:41:52 +0000

    Register post to admin site and publish test blog post

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
