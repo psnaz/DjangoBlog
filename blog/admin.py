f8b57cdacac8387b5c289ee91633e4ac524fd188
psnaz <psnazle@gmail.com>
2022-11-11 11:14:09 +0000

    Add slug auto-generation and filter to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -5,5 +5,7 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-
+    
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
     summernote_fields = ('content')
